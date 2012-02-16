a157aac7cf17bb2195b6bfbb6eb2c5ac0f1c53f5
Jonathan Garay <jonathan.garay@crowdint.com>
2012-02-16 07:12:06 -0600

    *Add: basic  test

--- test/test_paypal_fx.rb
+++ test/test_paypal_fx.rb
@@ -2,4 +2,13 @@ require 'helper'
 
 class TestPaypalFx < Test::Unit::TestCase
 
+  should "have commit method" do
+    assert PaypalFx.new({}).respond_to? :commit
+  end
+
+  should "have convert method" do
+    assert PaypalFx.new({}).respond_to? :convert
+  end
+
+
 end
