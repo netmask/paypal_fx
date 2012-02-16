8a3d53ba58279a9c5de5a050e9cb3fe56d74c76c
Jonathan Garay <jonathan.garay@crowdint.com>
2012-02-16 06:56:12 -0600

    Fixed: README

--- lib/paypal_fx.rb
+++ lib/paypal_fx.rb
@@ -6,10 +6,10 @@ require 'uri'
 
 class PaypalFx
 
-  SERVER = "svcs.sandbox.paypal.com"
-  SANDBOX = "svcs.sandbox.paypal.com"
+  SERVER = 'svcs.sandbox.paypal.com'
+  SANDBOX = 'svcs.sandbox.paypal.com'
 
-  CLIENT_INFO = { VERSION: "60.0", SOURCE: "PayPalRubySDKV1.2.0" }
+  CLIENT_INFO = { VERSION: '60.0', SOURCE: 'PayPalRubySDKV1.2.0' }
 
   attr_accessor :params
 
@@ -28,13 +28,13 @@ class PaypalFx
   # Converts an amount from origin currency to destination
   def convert(amount, origin, destination)
     request = {
-        "requestEnvelope.errorLanguage" => "en_US",
-        "baseAmountList.currency(0).amount" => amount,
-        "baseAmountList.currency(0).code" => origin,
-        "convertToCurrencyList.currencyCode(0)" => destination
+        'requestEnvelope.errorLanguage' => 'en_US',
+        'baseAmountList.currency(0).amount' => amount,
+        'baseAmountList.currency(0).code' => origin,
+        'convertToCurrencyList.currencyCode(0)' => destination
     }
 
-    commit "/AdaptivePayments/ConvertCurrency", request
+    commit '/AdaptivePayments/ConvertCurrency', request
   end
 
   # Sends the request to specified end point for paypal SANDBOX or SERVER
@@ -44,18 +44,18 @@ class PaypalFx
   # @return [Hash]  with the responce parameters
   def commit(endpoint, request)
 
-    headers = {"Content-Type" => "html/text",
-               "X-PAYPAL-SERVICE-VERSION" => "1.0.0",
-               "X-PAYPAL-SECURITY-USERID" => self.params[:user_id],
-               "X-PAYPAL-SECURITY-PASSWORD" => self.params[:password],
-               "X-PAYPAL-SECURITY-SIGNATURE" => self.params[:signature],
-               "X-PAYPAL-APPLICATION-ID" => self.params[:app_id],
-               "X-PAYPAL-DEVICE-IPADDRESS" => self.params[:ip_address] || "",
-               "X-PAYPAL-REQUEST-DATA-FORMAT" => "NV",
-               "X-PAYPAL-RESPONSE-DATA-FORMAT" => "NV"}
+    headers = {'Content-Type' => 'html/text',
+               'X-PAYPAL-SERVICE-VERSION' => '1.0.0',
+               'X-PAYPAL-SECURITY-USERID' => self.params[:user_id],
+               'X-PAYPAL-SECURITY-PASSWORD' => self.params[:password],
+               'X-PAYPAL-SECURITY-SIGNATURE' => self.params[:signature],
+               'X-PAYPAL-APPLICATION-ID' => self.params[:app_id],
+               'X-PAYPAL-DEVICE-IPADDRESS' => self.params[:ip_address] || '',
+               'X-PAYPAL-REQUEST-DATA-FORMAT' => 'NV',
+               'X-PAYPAL-RESPONSE-DATA-FORMAT' => 'NV'}
 
 
-    request_params = request.merge(CLIENT_INFO).map { |k, v| "#{k}=#{CGI.escape(v)}" }.join("&")
+    request_params = request.merge(CLIENT_INFO).map { |k, v| "#{k}=#{CGI.escape(v)}" }.join('&')
 
     http = Net::HTTP.new(params[:test] ? SANDBOX : SERVER, 443)
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -72,8 +72,8 @@ class PaypalFx
     #i hate my self for this
     h = {}
     hash.each do |k, v|
-      val = k.split(".").map { |k| k.tr('^A-Za-z0-9', '') }
-      key = val.count == 2 ? val[1] : val[val.count-2..val.count-1].join("_")
+      val = k.split('.').map { |k| k.tr('^A-Za-z0-9', '') }
+      key = val.count == 2 ? val[1] : val[val.count-2..val.count-1].join('_')
       h[key.to_sym] = v[0]
     end
     h
